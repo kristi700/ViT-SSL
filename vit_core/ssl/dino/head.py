1b42aac3d7bd7f07c07e3e962302afab9cc65f0d
kristi700 <kristof.tajti@gmail.com>
2025-07-09 14:18:25 +0200

    Deprecated import update

--- vit_core/ssl/dino/head.py
+++ vit_core/ssl/dino/head.py
@@ -1,7 +1,7 @@
 import torch.nn.functional as F
 
 from torch import nn
-from torch.nn.utils import weight_norm
+from torch.nn.utils.parametrizations import weight_norm
 
 
 class DINOHead(nn.Module):
